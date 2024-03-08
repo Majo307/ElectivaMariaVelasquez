de9a23e31f68c0f69b102f90090d8eff591a03d1
Maria Jose <mariajose72003@gmail.com>
2024-03-08 18:27:00 -0500

    ADD-creacion de los estilos

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ export const Header = () => {
+  return (
+    <div className='banner'>
+          
+      <h1 className='titulo'>Explora tu Gif</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  Webpagge  from './Webpagge';
+import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
